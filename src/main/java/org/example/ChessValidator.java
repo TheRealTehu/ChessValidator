6933296833adb98f8c980043afb01ed559d5719f
TheRealTehu <levex11@gmail.com>
2022-12-05 10:31:43 +0100

    add board and piece names

---
+++ src/main/java/org/example/ChessValidator.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class ChessValidator {
+    private char[][] board;
+
+    private final char WHITE_KING = 'k';
+    private final char BLACK_KING = 'K';
+    private final char WHITE_QUEEN = 'v';
+    private final char BLACK_QUEEN = 'V';
+    private final char WHITE_ROOK = 'b';
+    private final char BLACK_ROOK = 'B';
+    private final char WHITE_BISHOP = 'f';
+    private final char BLACK_BISHOP = 'F';
+    private final char WHITE_KNIGHT = 'h';
+    private final char BLACK_KNIGHT = 'H';
+    private final char WHITE_PAWN = 'g';
+    private final char BLACK_PAWN = 'G';
+}
