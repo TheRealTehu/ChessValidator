feaa630bbe707aae07d08c4a835278bbcf284322
Szentirmai-Schon Adam <adamsan3@gmail.com>
2022-12-06 20:44:17 +0100

    add validator test cases based on Main

--- src/test/java/org/example/ChessValidatorTest.java
+++ src/test/java/org/example/ChessValidatorTest.java
@@ -7,11 +7,58 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ChessValidatorTest {
 
+    ChessValidator validator = new ChessValidator();
+
     @BeforeEach
     void setUp() {
     }
 
     @Test
-    void isValidState() {
+    void given_KingsAreNextToEachOther_When_isValidState_Then_ReturnFalse() {
+        char[][] board = convert("""
+                ........
+                ........
+                ........
+                ........
+                ........
+                ........
+                ........
+                ......Kk""");
+        assertFalse(validator.isValidState(board));
+    }
+
+    @Test
+    void given_PlayerHasMoreKings_When_isValidState_Then_ReturnFalse() {
+        char[][] board = convert("""
+                bhfvkfhb
+                vvvvvvv.
+                ........
+                ........
+                .k..K...
+                ........
+                ........
+                ........""");
+        assertFalse(validator.isValidState(board));
+    }
+    @Test
+    void given_normalState1_When_isValidState_Then_ReturnTrue() {
+        char[][] board = convert("""
+                bhfvkfhb
+                vvvvvvvv
+                ........
+                ........
+                ....K...
+                ........
+                ........
+                ........""");
+        assertFalse(validator.isValidState(board));
+    }
+    private char[][] convert(String field) {
+        String[] lines = field.split("\n ");
+        char[][] result = new char[lines.length][];
+        for (int i = 0; i < lines.length; i++) {
+            result[i] = lines[i].toCharArray();
+        }
+        return result;
     }
 }
